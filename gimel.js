b769e858692472ea36abd109119ba6ba398a1fd8
jdborowy <jeandaniel.borowy@gmail.com>
2014-06-09 15:18:38 +0200

    SNAPSHOT

--- gimel.js
+++ gimel.js
@@ -1,5 +1,6 @@
 'use strict';
-var gimel = (function(rootScope) {
+
+var gimel = (function() {
     var Gimel = function Gimel() {
         this.utils = {
                       /**
@@ -18,6 +19,10 @@ var gimel = (function(rootScope) {
 
         this.modules = {};
 
+        /**
+         * The Module container class
+         * @class GimelModule
+         */
         var GimelModule = function GimelModule(name, dependencies, initializer) {
             this.name = name;
             this.dependencies = dependencies;
@@ -36,7 +41,7 @@ var gimel = (function(rootScope) {
 
         /**
          * Tests if some module dependencies are unknown
-         * @param {Object<GimelModules>} the defined modules
+         * @param {Object} the defined modules
          * @return {boolean} true if some mudules depend each other
          */
         var modulesDependenciesUnknown = function(modules) {
@@ -56,22 +61,24 @@ var gimel = (function(rootScope) {
 
         /**
          * Tests if the modules dependencies contains cycles (graph cycle detection algorithm)
-         * @param {Object<GimelModules>} the defined modules
+         * @param {Object} the defined modules
          * @return {boolean} true if some mudules depend each other
          */
         var modulesContainCycles = function(modules) {
-            var moduleName;
+            var moduleName = '';
             var colors = {};
             var nodeNumber = 0;
             var nodeQueue = [];
-
+            var dependencies = [];
+            var i = 0, ii = 0;
+            
             for (moduleName in modules) {
                 colors[moduleName] = 0;
             }
 
             for (moduleName in modules) {
-                var dependencies = modules[moduleName].dependencies;
-                for (var i = 0, ii = dependencies.length; i < ii; ++i) {
+                dependencies = modules[moduleName].dependencies;
+                for (i = 0, ii = dependencies.length; i < ii; ++i) {
                     ++colors[dependencies[i]];
                 }
             }
@@ -85,8 +92,8 @@ var gimel = (function(rootScope) {
 
             while (nodeQueue.length !== 0) {
                 moduleName = nodeQueue.shift();
-                var dependencies = modules[moduleName].dependencies;
-                for (var i = 0, ii = dependencies.length; i < ii; ++i) {
+                dependencies = modules[moduleName].dependencies;
+                for (i = 0, ii = dependencies.length; i < ii; ++i) {
                     if (--colors[dependencies[i]] === 0) {
                         nodeQueue.push(dependencies[i]);
                         ++nodeNumber;
@@ -99,7 +106,7 @@ var gimel = (function(rootScope) {
         /**
          * Creates a new gimel module
          * @param {string} name the module name
-         * @param {array<string>} dependencies the names of required modules to define this one
+         * @param {string[]} dependencies the names of required modules to define this one
          * @param {function} initializer the module definition function
          */
         var createModule = function(name, dependencies, initializer) {
@@ -121,7 +128,7 @@ var gimel = (function(rootScope) {
         /**
          * Defines a new gimel module
          * @param {string} name the module name
-         * @param {array<string>} dependencies the names of required modules to define this one
+         * @param {string[]} dependencies the names of required modules to define this one
          * @param {function} initializer the module definition function
          */
         this.defineModule = function(name, dependencies, initializer) {
@@ -145,7 +152,7 @@ var gimel = (function(rootScope) {
         /**
          * Get an ImageTemplate module
          * @param {string} name the module name
-         * @param {array<string>} dependencies the names of required modules to define this one
+         * @param {string[]} dependencies the names of required modules to define this one
          * @param {function} initializer the module definition function
          */
         this.module = function(name) {
@@ -161,14 +168,16 @@ var gimel = (function(rootScope) {
 
         /**
          * Sorts the module to initialize the dependencies before each module
-         * @param {array<GimelModule>} the modules in the right order
+         * @param {GimelModule[]} the modules in the right order
          */
         var buildInitWorkflow = function() {
             var unknownDependencies = modulesDependenciesUnknown(this.modules);
             var scheduledModules = [];
+            var i = 0, ii = 0;
+            var unresolvedDependencies = [];
 
             if (unknownDependencies.length > 0) { // Do we have unknown modules in dependencies ?
-                for (var i = 0, ii = unknownDependencies.length; i < ii; ++i) {
+                for (i = 0, ii = unknownDependencies.length; i < ii; ++i) {
                     console.error('Gimel::init: module "' + unknownDependencies[i] + '" is unknown');
                 }
             } else if (modulesContainCycles(this.modules)) { // Do we have cycles in dependencies ?
@@ -186,16 +195,16 @@ var gimel = (function(rootScope) {
                 while (modulesToSchedule.length !== 0) {
                     // Move the modules with resolved dependencies (module.modulesUnresolvedDependencies === [])
                     var tmpModules = [];
-                    for (var i = 0; i < modulesToSchedule.length; ++i) {
-                        var unresolvedDependencies = modulesUnresolvedDependencies[modulesToSchedule[i].name];
+                    for (i = 0; i < modulesToSchedule.length; ++i) {
+                        unresolvedDependencies = modulesUnresolvedDependencies[modulesToSchedule[i].name];
                         if (unresolvedDependencies.length === 0) {
                             tmpModules.push(modulesToSchedule.splice(i--, 1)[0]);
                             // We do i-- because the splice remove element i so the next element is at index i now
                         }
                     }
                     // Mark this modules as resolved in the other module dependencies
-                    for (var i = 0, ii = modulesToSchedule.length; i < ii; ++i) {
-                        var unresolvedDependencies = modulesUnresolvedDependencies[modulesToSchedule[i].name];
+                    for (i = 0, ii = modulesToSchedule.length; i < ii; ++i) {
+                        unresolvedDependencies = modulesUnresolvedDependencies[modulesToSchedule[i].name];
                         for (var j = 0, jj = tmpModules.length; j < jj; ++j) {
                             var index = unresolvedDependencies.indexOf(tmpModules[j].name);
                             if (index > -1) {
@@ -234,7 +243,7 @@ var gimel = (function(rootScope) {
     };
 
     return new Gimel();
-})(this);
+})();
 
 gimel.defineModule('imageTemplate', [], function(moduleContent, extensions) {
     moduleContent.dataTypes = {
@@ -252,6 +261,7 @@ gimel.defineModule('imageTemplate', [], function(moduleContent, extensions) {
     var ImageTemplate = function(ArrayContructor, channels) {
         /**
          * Constructs Image with specified width, height, channels and data.
+         * @class GimelImage
          * @constructor
          * @param {integer} width
          * @param {integer} height
@@ -268,6 +278,9 @@ gimel.defineModule('imageTemplate', [], function(moduleContent, extensions) {
             this.dy = width*channels;
             this.length = width*height*channels;
         };
+
+        GimelImage.prototype.CHANNELS = channels;
+        GimelImage.prototype.DATA_TYPE = type;
 
         moduleContent.structures.push(GimelImage);
         gimel[type + channels + 'ChImage'] = GimelImage;
@@ -378,7 +391,7 @@ gimel.module('imageTemplate').extend(function(moduleContent) {
          */
         GimelImage.prototype.normalize = function(srcMin, srcMax, destMin, destMax) {
             var thisData = this.data;
-            var alpha = (destMax - destMin)/(srcMax - srcMin)
+            var alpha = (destMax - destMin)/(srcMax - srcMin);
 
             for (var t = 0, tt = this.length; t < tt; t += 4) {
                 thisData[t] = (thisData[t] - srcMin)*alpha + destMin;
@@ -453,6 +466,143 @@ gimel.module('imageTemplate').extend(function(moduleContent) {
 });
 
 gimel.module('imageTemplate').extend(function(moduleContent) {
+    gimel.imageTemplate.extend(function(GimelImage) {
+        /**
+         * Convolve the image with the given kernel
+         * @param {IntMask} kernel mask (image with one channel)
+         * @param {boolean} normalize 
+         * @return {GimelImage} convolved image
+         * @todo handle the sides
+         */
+        GimelImage.prototype.convolve = function(kernel, normalize) {
+            console.assert(kernel.CHANNELS === 1, 'AbstractImage::convolute: Mask must have just one channel');
+            console.assert(kernel.width === kernel.height, 'AbstractImage::convolute: Mask must be square');
+
+            var convolvedImage = this.cloneStructure();
+            var destData = convolvedImage.data;
+            var srcData = this.data;
+            var t = 0, tt = 0, tSrc = 0;
+
+            var uu = kernel.width;
+            var vv = kernel.height;
+            var uu2 = kernel.width >> 1;
+            var vv2 = kernel.height >> 1;
+
+            var dx = this.dx;
+            var dy = this.dy;
+            var xx = this.width;
+            var yy = this.height;
+            var xx2 = xx - uu2;
+            var yy2 = yy - vv2;
+
+            var srcX, srcY;
+            var u, v, x, y;
+            var muv, offset;
+
+            for (v = 0; v < vv; ++v) {
+                for (u = 0; u < uu; ++u) {
+                    muv = kernel.data[v*kernel.width + u];
+                    for (y = 0; y < vv2; ++y) {
+                        for (x = 0; x < xx2; ++x) {
+                            srcX = x + u - uu2;
+                            srcY = y + v - vv2;
+                            tSrc = x*dx + y*dy;
+                            t = (srcX < 0 ? 1 - srcX : srcX)*dx + (srcY < 0 ? 1 - srcY : srcY)*dy;
+                            destData[tSrc] += srcData[t]*muv;
+                            destData[tSrc + 1] += srcData[t + 1]*muv;
+                            destData[tSrc + 2] += srcData[t + 2]*muv;
+                        }
+                    }
+
+                    for (y = yy2; y < yy; ++y) {
+                        for (x = 0; x < xx2; ++x) {
+                            srcX = x + u - uu2;
+                            srcY = y + v - vv2;
+                            tSrc = x*dx + y*dy;
+                            t = (srcX < 0 ? 1 - srcX : srcX)*dx + (srcY < yy ? srcY : 2*yy - srcY - 1)*dy;
+                            destData[tSrc] += srcData[t]*muv;
+                            destData[tSrc + 1] += srcData[t + 1]*muv;
+                            destData[tSrc + 2] += srcData[t + 2]*muv;
+                        }
+                    }
+
+                    for (y = vv2; y < yy2; ++y) {
+                        for (x = 0; x < uu2; ++x) {
+                            srcX = x + u - uu2;
+                            srcY = y + v - vv2;
+                            tSrc = x*dx + y*dy;
+                            t = (srcX < 0 ? 1 - srcX : srcX)*dx + (srcY < 0 ? 1 - srcY : srcY)*dy;
+                            destData[tSrc] += srcData[t]*muv;
+                            destData[tSrc + 1] += srcData[t + 1]*muv;
+                            destData[tSrc + 2] += srcData[t + 2]*muv;
+                        }
+                    }
+
+                    for (y = 0; y < yy2; ++y) {
+                        for (x = xx2; x < xx; ++x) {
+                            srcX = x + u - uu2;
+                            srcY = y + v - vv2;
+                            tSrc = x*dx + y*dy;
+                            t = (srcX < xx ? srcX : 2*xx - srcX - 1)*dx + (srcY < 0 ? 1 - srcY : srcY)*dy;
+                            destData[tSrc] += srcData[t]*muv;
+                            destData[tSrc + 1] += srcData[t + 1]*muv;
+                            destData[tSrc + 2] += srcData[t + 2]*muv;
+                        }
+                    }
+                    
+                    for (y = yy2; y < yy; ++y) {
+                        for (x = xx2; x < xx; ++x) {
+                            srcX = x + u - uu2;
+                            srcY = y + v - vv2;
+                            tSrc = x*dx + y*dy;
+                            t = (srcX < xx ? srcX : 2*xx - srcX - 1)*dx + (srcY < yy ? srcY : 2*yy - srcY - 1)*dy;
+                            destData[tSrc] += srcData[t]*muv;
+                            destData[tSrc + 1] += srcData[t + 1]*muv;
+                            destData[tSrc + 2] += srcData[t + 2]*muv;
+                        }
+                    }
+                }
+            }
+
+            for (v = 0; v < vv; ++v) {
+                for (u = 0; u < uu; ++u) {
+                    muv = kernel.data[v*kernel.width + u];
+                    offset = (u - uu2)*dx + (v - vv2)*dy;
+                    for (y = vv2; y < yy2; ++y) {
+                        var t0 = y*xx;
+                        for (x = uu2; x < xx2; ++x) {
+                            t = (t0 + x)*4;
+                            destData[t] += srcData[t + offset]*muv;
+                            destData[t + 1] += srcData[t + offset + 1]*muv;
+                            destData[t + 2] += srcData[t + offset + 2]*muv;
+                        }
+                    }
+                }
+            }
+
+            if (normalize) {
+                var maskSum = 0;
+                for (t = 0, tt = kernel.length; t < tt; ++t) {
+                    maskSum += kernel.data[t];
+                }
+
+                if (maskSum !== 0) {
+                    for (t = 0, tt = this.length; t < tt; t += 4) {
+                        destData[t] /= maskSum;
+                        destData[t + 1] /= maskSum;
+                        destData[t + 2] /= maskSum;
+                    }
+                }
+            }
+
+            return convolvedImage;
+        };
+    });
+
+    return false;
+});
+
+gimel.module('imageTemplate').extend(function(moduleContent) {
     moduleContent.extend(function(GimelImage) {
         /**
          * Make an exact copy of an image
@@ -460,6 +610,14 @@ gimel.module('imageTemplate').extend(function(moduleContent) {
          */
         GimelImage.prototype.clone = function() {
             return new GimelImage(this.width, this.height, this.data);
+        };
+
+        /**
+         * Gives a new image with the same structure (width, height, channels)
+         * @return {GimelImage} this the new image
+         */
+        GimelImage.prototype.cloneStructure = function() {
+            return new GimelImage(this.width, this.height);
         };
 
         /**
@@ -486,6 +644,55 @@ gimel.module('imageTemplate').extend(function(moduleContent) {
             this.data.set(image.data);
             return this;
         };
+
+        if (GimelImage.prototype.CHANNELS === 1) {
+            /**
+             * Fill in a channel with a value
+             * @param {number} value the value to set to pixels
+             * @return {GimelImage} this image
+             */
+            GimelImage.prototype.fill = function(value) {
+                var dataDest = this.data;
+                for (var t = 0, tt = this.length; t < tt; ++t) {
+                    dataDest[t] = value;
+                }
+                return this;
+            };
+        } else if (GimelImage.prototype.CHANNELS === 4) {
+            /**
+             * Fill in an image with channel values
+             * @param {number} ch0 the first channel value
+             * @param {number} ch1 the second channel value
+             * @param {number} ch2 the third channel value
+             * @param {number} ch3 the fourth channel value
+             * @return {GimelImage} this image
+             */
+            GimelImage.prototype.fill = function(ch0, ch1, ch2, ch3) {
+                var dataDest = this.data;
+                for (var t = 0, tt = this.length; t < tt; t += 4) {
+                    dataDest[t] = ch0;
+                    dataDest[t + 1] = ch1;
+                    dataDest[t + 2] = ch2;
+                    dataDest[t + 3] = ch3;
+                }
+                return this;
+            };
+            
+            /**
+             * Fill in a channel with a value
+             * @param {integer} channel the channel index
+             * @param {number} value the value to set to channel
+             * @return {GimelImage} this image
+             */
+            GimelImage.prototype.fillChannel = function(channel, value) {
+                var dataDest = this.data;
+                for (var t = 0, tt = this.length; t < tt; t += 4) {
+                    dataDest[t + channel] = value;
+                }
+                return this;
+            };
+        }
+        
     });
 });
 
@@ -516,10 +723,20 @@ gimel.defineModule('io', ['imageTemplate'], function(moduleContent, extensions) 
         context.putImageData(this.canvasData, 0, 0);
     };
 
+    /**
+     * Create a Gimel image from a canvas DOM Element
+     * @param {HTMLCanvasElement} canvasDomElement the canvas DOM Element
+     * @return {GimelImage} the Gimel image
+     */
     moduleContent.imageFromDomCanvas = function(canvasDomElement) {
         return new gimel.CanvasImage(canvasDomElement.width, canvasDomElement.height, canvasDomElement);
     };
 
+    /**
+     * Create a Gimel image from a DOM image Element
+     * @param {Image} imageDomElement the image DOM Element
+     * @return {GimelImage} the Gimel image
+     */
     moduleContent.imageFromDomImage = function(imageDomElement) {
         var canvasDomElement = document.createElement('canvas');
         canvasDomElement.width = imageDomElement.width;
