19f55cf392a7a58d00db3c276bb431f2db8293e7
jdborowy <jeandaniel.borowy@gmail.com>
2014-06-16 23:24:56 +0200

    + Image View + operators on frequency domain

---
+++ src/image-template/image-view/main.js
@@ -0,0 +1,20 @@
+gimel.module('imageTemplate').extend(function(moduleContent) {
+    gimel.imageTemplate.extend(function(GimelImage, dataType, channels) {
+        GimelImage.ImageView = function ImageView(image, x, y, width, height) {
+            this.image = image;
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.dx = image.dx;
+            this.dy = image.dy;
+        };
+
+        GimelImage.ImageView.prototype.CHANNELS = GimelImage.prototype.CHANNELS;
+        GimelImage.ImageView.prototype.DATA_TYPE = GimelImage.prototype.DATA_TYPE;
+
+        GimelImage.prototype.getImageView = function(x, y, width, height) {
+            return new GimelImage.ImageView(this, x, y, width, height);
+        };
+    });
+});
---
+++ src/image-template/operators/frequency.js
@@ -0,0 +1,14 @@
+gimel.module('imageTemplate').extend(function(moduleContent) {
+    gimel.imageTemplate.extend(function(GimelImage, dataType, channels) {
+
+        GimelImage.prototype.haar = function(image) {
+            // TODO
+        };
+
+        GimelImage.prototype.fft = function(image) {
+            // TODO
+        };
+    });
+
+    return false;
+});
